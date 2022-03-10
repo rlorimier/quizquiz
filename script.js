fd9e2562725a69e8b13474547439cf9ea70b2326
rlorimier <rere.rnt@gmail.com>
2022-03-10 14:58:34 +0000

    add questions

--- script.js
+++ script.js
@@ -1,3 +1,106 @@
+//questions
+const q0 = {
+    numQuestao   : 0,
+    pergunta     : "Pergunta",
+    alternativaA : "Alternativa A",
+    alternativaB : "Alternativa B",
+    alternativaC : "Alternativa C",
+    correta      : "0",
+}
+
+const q1 = {
+    numQuestao   : 1,
+    pergunta     : "What is an Equinox:",
+    alternativaA : "The time when the plane of Earth's equator passes through the geometric center of the Sun's disk",
+    alternativaB : "The point where the Sun appears to reach either its highest or lowest point in the sky for the year",
+    alternativaC : "The effect caused by the interaction of charged particles from the Sun with atoms in the upper atmosphere",
+    correta      : "The time when the plane of Earth's equator passes through the geometric center of the Sun's disk",
+}
+
+const q2 = {
+    numQuestao   : 2,
+    pergunta     : "What NASA stands for:",
+    alternativaA : "National Aliance and Space Aeronautics",
+    alternativaB : "Nacional Administration and Space Aliance",
+    alternativaC : "National Aeronautics and Space Administration",
+    correta      : "National Aeronautics and Space Administration",
+}
+
+const q3 = {
+    numQuestao   : 3,
+    pergunta     : "The most common type of black holes, they can be up to 20 times more massive than the Sun:",
+    alternativaA : "Primordial Black Holes",
+    alternativaB : "Stellar Black Holes",
+    alternativaC : "Supermassive Black Holes",
+    correta      : "Stellar Black Holes",
+}
+
+const q4 = {
+    numQuestao   : 4,
+    pergunta     : "What is the deadliest planet in the Solar System?",
+    alternativaA : "Venus",
+    alternativaB : "Saturn",
+    alternativaC : "Mars",
+    correta      : "Venus",
+}
+
+const q5 = {
+    numQuestao   : 5,
+    pergunta     : "The Earth's average distance to the Sun is:",
+    alternativaA : "109 million km",
+    alternativaB : "149 million km",
+    alternativaC : "199 million km",
+    correta      : "149 million km",
+}
+
+const q6 = {
+    numQuestao   : 6,
+    pergunta     : "What is the Sun mainly made of?",
+    alternativaA : "Rock",
+    alternativaB : "Lava",
+    alternativaC : "Gas",
+    correta      : "Gas",
+}
+
+const q7 = {
+    numQuestao   : 7,
+    pergunta     : "How many moons does Mars have?",
+    alternativaA : "6",
+    alternativaB : "4",
+    alternativaC : "2",
+    correta      : "2",
+}
+
+const q8 = {
+    numQuestao   : 8,
+    pergunta     : "Icy bodies of frozen gases, rocks and dust left over from the formation of the solar system are:",
+    alternativaA : "Comets",
+    alternativaB : "Stars",
+    alternativaC : "Asteroids",
+    correta      : "Comets",
+}
+
+const q9 = {
+    numQuestao   : 9,
+    pergunta     : "When did Apollo-11 successfully land on the Moon?",
+    alternativaA : "1968",
+    alternativaB : "1969",
+    alternativaC : "1970",
+    correta      : "1969",
+}
+
+const q10 = {
+    numQuestao   : 10,
+    pergunta     : "Where is located the Asteroid Belt?",
+    alternativaA : "Between Mars and Jupiter",
+    alternativaB : "Between Mars and Earth",
+    alternativaC : "Between Earth and Jupiter",
+    correta      : "Between Earth and Jupiter"
+}
+
+// array with all questions
+const questoes = [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
+
 let titulo = document.querySelector('h1')
 let instrucoes = document.querySelector('#instrucoes')
 let aviso = document.querySelector('#aviso')
@@ -19,62 +122,6 @@ let articleQuestoes = document.querySelector('.questoes')
 // ol li com as alternativas
 let alternativas = document.querySelector('#alternativas')
 
-const q0 = {
-    numQuestao   : 0,
-    pergunta     : "Pergunta",
-    alternativaA : "Alternativa A",
-    alternativaB : "Alternativa B",
-    alternativaC : "Alternativa C",
-    correta      : "0",
-}
-
-const q1 = {
-    numQuestao   : 1,
-    pergunta     : "Boreal é o mesmo que...",
-    alternativaA : "Sul",
-    alternativaB : "Leste",
-    alternativaC : "Norte",
-    correta      : "Norte",
-}
-
-const q2 = {
-    numQuestao   : 2,
-    pergunta     : "Austral é o mesmo que...",
-    alternativaA : "Oeste",
-    alternativaB : "Sul",
-    alternativaC : "Norte",
-    correta      : "Sul",
-}
-
-const q3 = {
-    numQuestao   : 3,
-    pergunta     : "Nascente é o mesmo que...",
-    alternativaA : "Sul",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Leste",
-}
-
-const q4 = {
-    numQuestao   : 4,
-    pergunta     : "Poente é o mesmo que...",
-    alternativaA : "Norte",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Oeste",
-}
-
-const q5 = {
-    numQuestao   : 5,
-    pergunta     : "O Brasil se localiza em qual continente?",
-    alternativaA : "África",
-    alternativaB : "Europa",
-    alternativaC : "América",
-    correta      : "América",
-}
-
-// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
-const questoes = [q0, q1, q2, q3, q4, q5]
 
 let numero = document.querySelector('#numero')
 let total  = document.querySelector('#total')
@@ -185,8 +232,35 @@ function fimDoJogo() {
     // OCULTAR O ARTICLE DA QUESTAO
     articleQuestoes.style.display = 'none'
 
-    setTimeout(function() {
-        pontos = 0 // zerar placar
-        location.reload();
-    }, 2000)
+    
 }
+
+let quizQuestions = [
+    q0 = {
+        nQuestion: 0,
+        question: "Pergunta",
+        choiceA: "Alternativa A",
+        choiceB: "Alternativa B",
+        choiceC: "Alternativa C",
+        correctAnswer: "0",
+    },
+
+    q1 = {
+		nQuestion: 1,
+		question: "What is an Equinox:",
+        choiceA: "The time when the plane of Earth's equator passes through the geometric center of the Sun's disk",
+        choiceB: "The point where the Sun appears to reach either its highest or lowest point in the sky for the year",
+        choiceC: "The effect caused by the interaction of charged particles from the Sun with atoms in the upper atmosphere",
+		correctAnswer: "The time when the plane of Earth's equator passes through the geometric center of the Sun's disk"
+	},
+        
+    q2 = {
+        nQuestion: 2,
+        question: "What NASA stands for:",
+        choiceA: "National Aliance and Space Aeronautics",
+        choiceB: "Nacional Administration and Space Aliance",
+        choiceC: "National Aeronautics and Space Administration",
+        correctAnswer: "National Aeronautics and Space Administration"
+    },
+    
+]
